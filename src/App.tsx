ba80fafe48364da3afbe8aa43502330b1a1eba33
dev-shab <shabreeshnair1998@gmail.com>
2026-06-15 12:21:41 +0530

    feat(setup): setup shadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div className="text-3xl">Voyageur</div>;
-};
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
 
 export default App;
