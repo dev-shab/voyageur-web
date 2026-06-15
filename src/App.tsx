a3a3556cc637970cdb7766fd90e05182d0ab30f5
dev-shab <shabreeshnair1998@gmail.com>
2026-06-15 15:16:29 +0530

    feat(setup): initialise project, install dependencies needed and skeleton setup (#1)
    * feat(setup): setup React+TS project
    
    * feat(setup): setup tailwind CSS
    
    * feat(setup): setup shadCN
    
    * feat(setup): setup theme mode toggle
    
    * feat(setup): setup basic skeleton - header and sidebar

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ThemeProvider } from "./components/theme-provider";
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+        <SidebarInset>
+          <SiteHeader />
+        </SidebarInset>
+      </SidebarProvider>
+    </ThemeProvider>
+  );
+}
